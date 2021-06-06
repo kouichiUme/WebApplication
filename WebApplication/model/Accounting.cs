ae6e6be62bdadeb4f95f70e963d19d32bfee8ca6
Kouichi Umezawa <kouichiume@gmail.com>
2021-06-07 02:07:59 +0900

    numsharp and pandas net

---
+++ WebApplication/model/Accounting.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.model
+{
+    public class Accounting
+    {
+
+        NumSharp.NDArray d = new NumSharp.NDArray();
+
+
+
+        PandasNet.IDataFrame data = new PandasNet.Impl.DataFrame<String>();
+
+
+    }
+}
