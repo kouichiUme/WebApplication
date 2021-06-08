a6491262687531b15a11f64cb64f7acbe4d72082
Kouichi Umezawa <kouichiume@gmail.com>
2021-06-08 23:22:02 +0900

    interval tree

---
+++ WebApplication/model/IntervalTree.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.model
+{
+    public class IntervalTree
+    {
+
+
+
+        class Interval
+        {
+            public int low;
+            public int high;
+        }
+
+
+        public IntervalTree()
+        {
+            IntervalTree.Interval interval = new IntervalTree.Interval();
+
+
+        }
+
+
+
+        
+       
+
+
+
+    }
+
+
+
+
+}
