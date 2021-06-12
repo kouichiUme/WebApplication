4b5b2046074d277ead12222777e6a500dc5744b3
Kouichi Umezawa <kouichiume@gmail.com>
2021-06-12 23:56:39 +0900

    use collection library

---
+++ WebApplication/model/RodCutProblem.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.model
+{
+    public class RodCutProblem
+    {
+
+
+        public RodCutProblem()
+        {
+
+
+            SortedDictionary<string, string> openWith = new SortedDictionary<string, string>();
+
+
+
+        }
+
+
+    }
+}
