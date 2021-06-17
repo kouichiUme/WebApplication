9b218f021ba563276e633c6da5a1d541eedeb0ee
Kouichi Umezawa <kouichiume@gmail.com>
2021-06-17 22:58:45 +0900

    いまいちわからない・・・

--- WebApplication/model/DynamicPlanning.cs
+++ WebApplication/model/DynamicPlanning.cs
@@ -133,5 +133,69 @@ namespace WebApplication.model
         }
 
 
+        public void longestCommonSubsequence(int[] x , int[] y)
+        {
+            int m = x.Length;
+            int n = y.Length;
+
+            char[][] b = new char[m][];
+            int[][] c = new int[m][];
+
+            for(int i = 1; i<m; i++)
+            {
+                c[i][0] = 0;
+            }
+            for(int i = 0; i < n; ++i)
+            {
+                c[0][i] =0;
+            }
+
+
+            for(int i =1; i<m; ++i)
+            {
+
+                for(int j = 1; j < n; ++j)
+                {
+
+
+
+                    if (x[i]==y[j])
+                    {
+                        c[i][j] = c[i-1][j-1]+1;
+                        b[i][j] = 'h';
+
+
+
+                    }else if(c[i-1][j] >=c[i][j-1]){
+
+                        c[i][j] =c[i-1][j];
+                        b[i][j] = 'u';
+
+
+
+                    }
+                    else
+                    {
+                        c[i][j] = c[i][j-1];
+                        b[i][j] = 'l';
+
+                    }
+
+                }
+
+
+
+
+
+            }
+
+            // c,bが結果になる
+
+
+            return ;
+
+        }
     }
+
+
 }
