2e7c7241c36251fb22638bfd1df16845627156b1
Kouichi Umezawa <kouichiume@gmail.com>
2021-07-13 22:42:38 +0900

    graph theory

---
+++ WebApplication/model/GraphTheory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.model
+{
+    public class GraphTheory<T> where T : new()
+
+    {
+
+        LinkedList<T> list;
+
+
+
+        public GraphTheory(T t)
+        {
+            this.list = new LinkedList<T>();
+            list.AddLast(t);
+
+        }
+
+
+
+        
+
+    }
+}
